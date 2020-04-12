5c9003fd2e0d7d2e1d41b4a2e9ba69652204acd4
EYE MOTION <eyemotiongamestudio@gmail.com>
2020-04-12 15:36:45 +0400

    upload

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-function Pagination(gotoNextPage, gotoPrevPage) {
+function Pagination({gotoNextPage, gotoPrevPage}) {
     return(
         <div>
-            {gotoPrevPage && <button onClick={gotoPrevPage}>Previous</button>}
-            {gotoNextPage && <button onClick={gotoNextPage}>Next</button>}
+            {gotoPrevPage && <button className="btn btn-secondary" style={{margin: "8px"}} onClick={gotoPrevPage}>Previous</button>}
+            {gotoNextPage && <button className="btn btn-info" style={{margin: "8px"}} onClick={gotoNextPage}>Next</button>}
             
         </div>
     )
 }
 export default Pagination
+
+
